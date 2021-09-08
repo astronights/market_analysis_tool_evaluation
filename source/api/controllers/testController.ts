32591abf43421a8da7cf22787d541d1adfe17505
astronights <shubhankar.a31@gmail.com>
2021-09-08 23:05:25 +0800

    Basic Node App set up with test router

---
+++ source/api/controllers/testController.ts
@@ -0,0 +1,20 @@
+import { Request, Response, Router } from 'express';
+
+export class TestController {
+
+    path = '/test';
+    router = Router();
+
+    constructor() {
+        this.router.get(this.path, this.getMessage);
+        this.router.post(this.path, this.submitName);
+    }
+
+    public async getMessage(req: Request, res: Response): Promise<Response> {
+        return res.status(200).json({"message": "Hello and welcome to the app"});
+    }
+
+    public async submitName(req: Request, res: Response): Promise<Response> {
+        return res.status(200).json({"message": `Hello ${req.query.name}`})
+    }
+}
--- source/app.ts
+++ source/app.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import { createServer } from 'http';
 import './config';
 
+import router from './api/router';
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use('/', router);
 
 const HOST = process.env.host || "127.0.0.1";
 const PORT = parseInt(process.env.PORT || "3000");
