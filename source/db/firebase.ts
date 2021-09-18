e2a27e09ecb311841b2f57017a2af6a8f3e036b7
astronights <shubhankar.a31@gmail.com>
2021-09-18 18:30:21 +0800

    Minor cleanup

--- source/db/firebase.ts
+++
@@ -1,9 +0,0 @@
-import admin from "firebase-admin";
-
-var serviceAccount = require("../../mate-db-firebase-adminsdk-vekns-52206f7281.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-export const firebase_db = admin.firestore();
