df47a2da3dd7c7e3c227793f865b8fa3d7f2d97c
astronights <shubhankar.a31@gmail.com>
2021-09-19 19:27:21 +0800

    Added base routing

--- source/api/router.ts
+++ source/api/router.ts
@@ -10,5 +10,8 @@ const ohlcv = new OHLCVController();
 
 router.use("/test", testApis.router);
 router.use("/ohlcv", ohlcv.router);
+router.get("/", (req, res) => {
+  res.status(200).json("Connected!");
+});
 
 export default router;
--- source/config.ts
+++ source/config.ts
@@ -4,7 +4,7 @@ import { config } from "dotenv";
 config({ path: resolve(__dirname, "../.env") });
 
 const HOST = process.env.HOST || "127.0.0.1";
-const PORT = process.env.PORT || "3000";
+const PORT = process.env.PORT || "8080";
 const MARKET = process.env.MARKET;
 const CURRENCY = process.env.CURRENCY;
 const PERIOD = parseInt(process.env.PERIOD || "3600");
